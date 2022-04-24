5116bd6c9157da66485a7cba52998208ee292790
mehdi-avara <avaramehdi@gmail.com>
2022-04-24 16:53:26 +0430

    min 16 of video

--- lib/Models/Task.dart
+++ lib/Models/Task.dart
@@ -1,11 +1,35 @@
 class Task{
-  String name;
+  String title;
   String description;
   String date;
   bool isDone;
-  Task({this.name, this.description, this.date, this.isDone});
+  Task({this.title, this.description, this.date, this.isDone});
+  void setTitle(String title){
+    this.title = title;
+  }
+  void setDescription(String description){
+    this.description = description;
+  }
+  void setDate(String date){
+    this.date = date;
+  }
+  void setIsDone(bool isDone){
+    this.isDone = isDone;
+  }
+  String getTitle(){
+    return this.title;
+  }
+  String getDescription(){
+    return this.description;
+  }
+  String getDate(){
+    return this.date;
+  }
+  bool getIsDone(){
+    return this.isDone;
+  }
   @override
   String toString() {
-    return 'Task{name: $name, description: $description, date: $date, isDone: $isDone}';
+    return 'Task{Title: $title, description: $description, date: $date, isDone: $isDone}';
   }
 }
--- lib/pages/ToDoList.dart
+++ lib/pages/ToDoList.dart
@@ -1,13 +1,46 @@
-
 import 'package:flutter/material.dart';
+import 'package:todolist/Models/Task.dart';
 
 class ToDoList extends StatelessWidget {
-  const ToDoList({Key key}) : super(key: key);
+  // const ToDoList({Key key}) : super(key: key);
+  List<Task> tasks = [
+    Task(
+      title: 'Task 1',
+      description: 'Description 1',
+      date: DateTime.now().toString(),
+      isDone: false,
+    ),
+    Task(
+      title: 'Task 2',
+      description: 'Description 2',
+      date: DateTime.now().toString(),
+      isDone: false,
+    ),
+    Task(
+      title: 'Task 3',
+      description: 'Description 3',
+      date: DateTime.now().toString(),
+      isDone: false,
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('ToDoList'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ToDoList'),
+      ),
+      body: Container(
+        child: ListView.builder(
+            itemCount: tasks.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(tasks[index].title),
+                subtitle: Text(tasks[index].description),
+                trailing: Icon(Icons.arrow_forward_ios),
+              );
+            }),
+      ),
     );
   }
 }
